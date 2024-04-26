50782e0a4964386f6955e532917f79929ad8ff4a
neo.chen <neocxf@gmail.com>
2024-04-26 20:49:58 +0800

    feat: add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function page() {
-  return <p>Customers Page</p>;
+import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+export default async function page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-center">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <Suspense key={query + currentPage}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -194,24 +194,41 @@ export async function fetchCustomers() {
   }
 }
 
+export async function fetchCustomersPages(query: string) {
+  noStore();
+  try {
+    const count = await sql`SELECT count(*)
+                                FROM customers
+                                WHERE customers.name ILIKE ${`%${query}%`}
+                                   OR customers.email ILIKE ${`%${query}%`}
+
+
+        `;
+    return Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of customers.');
+  }
+}
+
 export async function fetchFilteredCustomers(query: string) {
   noStore();
   try {
     const data = await sql<CustomersTableType>`
-            SELECT customers.id,
-                   customers.name,
-                   customers.email,
-                   customers.image_url,
-                   COUNT(invoices.id)                                                         AS total_invoices,
-                   SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-                   SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END)    AS total_paid
-            FROM customers
-                     LEFT JOIN invoices ON customers.id = invoices.customer_id
-            WHERE customers.name ILIKE ${`%${query}%`}
-               OR customers.email ILIKE ${`%${query}%`}
-            GROUP BY customers.id, customers.name, customers.email, customers.image_url
-            ORDER BY customers.name ASC
-        `;
+      SELECT customers.id,
+             customers.name,
+             customers.email,
+             customers.image_url,
+             COUNT(invoices.id)                                                         AS total_invoices,
+             SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+             SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END)    AS total_paid
+      FROM customers
+             LEFT JOIN invoices ON customers.id = invoices.customer_id
+      WHERE customers.name ILIKE ${`%${query}%`}
+         OR customers.email ILIKE ${`%${query}%`}
+      GROUP BY customers.id, customers.name, customers.email, customers.image_url
+      ORDER BY customers.name ASC
+    `;
 
     const customers = data.rows.map((customer) => ({
       ...customer,
